95119e17418b1eaab6370c45fe27acf8db9a2ab4
muhammadarash1997 <muhammadarash1997@gmail.com>
2022-11-13 11:55:23 +0700

    First commit

---
+++ bin/class.dart
@@ -0,0 +1,15 @@
+class Person {
+    String name = "Guest";
+    String? address;
+    final String country = "Indonesia";
+}
+
+void main() {
+    var person1 = Person();
+    print(person1.name);
+    print(person1.address);
+    print(person1.country);
+
+    Person person2 = Person();
+    print(person2);
+}
---
+++ bin/static_field.dart
@@ -0,0 +1,9 @@
+class Application {
+  static final String name = 'Belajar Dart OOP';
+  static final String author = 'Eko Kurniawan';
+}
+
+void main(){
+  print(Application.name);
+  print(Application.author);
+}
